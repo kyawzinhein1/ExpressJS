3d943fbd95fb2f23a0fd5983bc390380e0ba302a
unknown <kyawzinhein162@gmail.com>
2024-03-09 16:13:48 +0630

    association

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 
 const sequelize = require("./util/database");
 
+const Post = require("./models/post");
+const User = require("./models/user");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -15,6 +18,16 @@ const adminRoutes = require("./routes/admin");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      // console.log(user);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/post", (req, res, next) => {
   console.log("I am post middleware");
@@ -34,10 +47,22 @@ app.use("/admin", (req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(postRoutes);
 
+Post.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Post);
+
 sequelize
   .sync()
   .then((result) => {
-    console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Admin", email: "admin@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(8080);
   })
   .catch((err) => console.log(err));
--- controllers/posts.js
+++ controllers/posts.js
@@ -4,11 +4,12 @@ const Post = require("../models/post");
 
 exports.createPost = (req, res) => {
   const { title, description, photo } = req.body;
-  Post.create({
-    title,
-    description,
-    imgUrl: photo,
-  })
+  req.user
+    .createPost({
+      title,
+      description,
+      imgUrl: photo,
+    })
     .then((result) => {
       console.log(result);
       console.log("Post create success");
