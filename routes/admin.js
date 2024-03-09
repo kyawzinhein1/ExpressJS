5a96e0a3425fbc75836f3b9a8ae124c3086d4e10
unknown <kyawzinhein162@gmail.com>
2024-03-09 15:01:49 +0630

    CRUD finish

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,18 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/posts");
 
+// /admin/create-post
 router.get("/create-post", postController.renderCreatePage);
 
 router.post("/", postController.createPost);
+
+// /admin/post/id
+router.post("/post/:postId", postController.deletePost);
+
+// admin/post-edit
+router.post("/post-edit", postController.updatePost);
+
+// admin/post-edit/id
+router.get("/post-edit/:postId", postController.getOldPost);
 
 module.exports = router;
